9692dc57b404957f41cb58d234530fad199032bd
Peter Cooner <petriborg@gmail.com>
2022-06-26 11:47:13 -0700

    improve loggen control

--- test/loggen.py
+++ test/loggen.py
@@ -13,10 +13,17 @@ from datetime import datetime
 log = logging.getLogger()
 
 
-def get_words():
-    word_file = "/usr/share/dict/words"
+def get_words(words_dict_fn):
     try:
-        with open(word_file) as fh:
+        # read cache file if possible
+        with open(words_dict_fn) as fh:
+            return fh.read().splitlines()
+    except IOError:
+        pass
+
+    global_word_file = "/usr/share/dict/words"
+    try:
+        with open(global_word_file) as fh:
             return fh.read().splitlines()
     except IOError:
         pass
@@ -27,6 +34,11 @@ def get_words():
     try:
         rsp = requests.get(word_site, headers={'user-agent': 'curl/7'})
         if rsp.ok:
+            try:
+                with open(words_dict_fn, 'wb') as fh:
+                    fh.write(rsp.content)
+            except IOError:
+                pass   # we don't care if we fail to write cache file
             return rsp.content.decode().splitlines()
         else:
             log.error('HTTP request failed: %s', rsp.reason)
@@ -39,19 +51,45 @@ def get_words():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--dt', '--datetime', default=False, action='store_true'
+        '-d', '--dt', '--datetime', default=False, action='store_true'
+    )
+    parser.add_argument(
+        '-s', '--sleep', default=.3, type=float,
+    )
+    parser.add_argument(
+        '--words-file', default='.loggen_words_dict', type=str,
+    )
+    parser.add_argument(
+        '--output-file', default=None, type=str,
     )
     options = parser.parse_args()
     use_dt = options.dt
 
-    dictionary = get_words()
-    while True:
-        time.sleep(.3)
-        line = (' '.join([random.choice(dictionary) for _ in range(5)]))
-        if use_dt:
-            dt = datetime.now().strftime('%b %d %H:%M:%S')
-            line = dt + ' ' + line
-        print(line, flush=True)
+    dictionary = get_words(options.words_file)
+    fh = None
+    try:
+        if options.output_file:
+            fh = open(options.output_file, 'w')
+        while True:
+            time.sleep(options.sleep)
+            line = (' '.join([random.choice(dictionary) for _ in range(5)]))
+            if use_dt:
+                dt = datetime.now().strftime('%b %d %H:%M:%S')
+                line = dt + ' ' + line
+            if fh:
+                fh.write(line)
+                fh.write('\n')
+                fh.flush()
+
+            print(line, flush=True)
+    except IOError:
+        pass
+    finally:
+        if options.output_file and fh:
+            try:
+                fh.close()
+            except IOError:
+                pass  # ignore close errors
 
 
 if __name__ == '__main__':
