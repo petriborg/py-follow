6884c9ebefa35bce6ebbcbdf90af985922c5cc8d
Peter Cooner <petriborg@gmail.com>
2022-06-26 11:49:48 -0700

    improve shell string handling

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ realpath() {
 }
 
 root=${0%/*}
-req=$(realpath ${root}/requirements.txt)
-venv=$(realpath ${root}/venv)
+req=$(realpath "${root}/requirements.txt")
+venv=$(realpath "${root}/venv")
 python3=$(command -v python3) || echo "could not find python 3"
 virtualenv=$(command -v virtualenv) || { echo "could not find python virtualenv"; exit 1; }
 PYTHON=${PYTHON:-$python3}  # if variable not set/null use $python3
@@ -20,17 +20,17 @@ fi
 echo "setup virtualenv for testing and devel (${venv})"
 echo "python: $PYTHON"
 echo "press enter to continue with setup..."
-read ENT
+read -r _
 
 echo "starting setup..."
-pushd $root >/dev/null
+pushd "$root" >/dev/null
 echo "creating virtualenv for $($PYTHON --version)..."
-$virtualenv -p ${PYTHON} --always-copy --no-download ${venv}
+$virtualenv -p "${PYTHON}" --always-copy --no-download "${venv}"
 
 echo "installing required packages..."
-source ${venv}/bin/activate
+source "${venv}/bin/activate"
 pip=$(command -v pip) || { echo "could not find pip in virtualenv"; exit 1; }
-$pip install -r $req
-$pip install -e .[test]
+$pip install -r "$req"
+$pip install -e ".[test]"
 popd >/dev/null
 
